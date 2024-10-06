43029fec59d087e04cfd14b718f43dd25a366790
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-10-06 22:53:58 +0530

    nextauth using google provider done

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,24 +1,33 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+
 export function Appbar() {
   const session = useSession();
   return (
-    <div>
-      <div className="flex justify-between">Muzi</div>
-      <div>
-        {/* //user is logged in  */}
-        {session.data?.user && (
-          <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>
-            Logout
-          </button>
-        )}
-        {/* //user is not properly logged in  */}
-        {!session.data?.user && (
-          <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>
-            SignIn
-          </button>
-        )}
+    <div className="bg-gray-800 p-4 text-white">
+      <div className="flex justify-between items-center">
+        <div className="text-xl font-bold">Muzi</div>
+        <div>
+          {/* user is logged in */}
+          {session.data?.user && (
+            <button
+              className="m-2 p-2 bg-blue-500 hover:bg-blue-600 text-white rounded"
+              onClick={() => signOut()}
+            >
+              Logout
+            </button>
+          )}
+          {/* user is not properly logged in */}
+          {!session.data?.user && (
+            <button
+              className="m-2 p-2 bg-blue-500 hover:bg-blue-600 text-white rounded"
+              onClick={() => signIn()}
+            >
+              Sign In
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
